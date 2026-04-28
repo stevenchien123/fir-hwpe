fee461c50fb146ce43833a65cc9c935368bcf47f
ChienShaoHsuan <p76131474@gs.ncku.edu.tw>
2026-04-28 22:39:15 +0800

    Add ENABLE_VIP to confirm the module has been included

--- rtl/fir_top_wrap.sv
+++ rtl/fir_top_wrap.sv
@@ -51,6 +51,15 @@ module fir_top_wrap
   output logic                                  periph_r_valid,
   output logic       [ID-1:0]                   periph_r_id
 );
+  `ifdef ENABLE_VIP
+    initial begin
+      #10us $info("Successfully integrated fir-hwpe module.");
+    end
+  `else
+    initial begin
+      #10us $fatal("fir-hwpe module is not enabled. Add the define flag to the package description (Bender.yml)");
+    end
+  `endif
 
   localparam hci_size_parameter_t `HCI_SIZE_PARAM(tcdm) = '{
     DW:  32,
